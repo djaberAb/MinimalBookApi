f5ecb5561dff6e7daf3288970936ee0a3abbaf60
Abbas Djaber <abbas_djaber@outlook.com>
2023-05-13 17:05:40 +0100

    none

--- MinimalBookApi/Program.cs
+++ MinimalBookApi/Program.cs
@@ -3,7 +3,6 @@ using MinimalBookApi;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>();
