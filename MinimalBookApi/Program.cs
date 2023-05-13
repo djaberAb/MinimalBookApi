3bf6a41406e17f981f2f3b60222733865e4a42a5
Abbas Djaber <abbas_djaber@outlook.com>
2023-05-13 17:17:37 +0100

    added comments

--- MinimalBookApi/Program.cs
+++ MinimalBookApi/Program.cs
@@ -7,25 +7,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>();
 
+// Create a new instance of the DataContext class
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Enable middleware to serve generated Swagger as a JSON endpoint.
     app.UseSwagger();
+    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+// Get all books
 app.MapGet("/book", async (DataContext context) =>
     await context.Books.ToListAsync());
 
+// Get a book by id
 app.MapGet("/book/{id}", async (DataContext context, int id) =>
     await context.Books.FindAsync(id) is Book book ?
         Results.Ok(book) :
         Results.NotFound("Sorry, book not found. :("));
 
+// Add a book
 app.MapPost("/book", async (DataContext context, Book book) =>
 {
     context.Books.Add(book);
@@ -33,6 +39,7 @@ app.MapPost("/book", async (DataContext context, Book book) =>
     return Results.Ok(await context.Books.ToListAsync());
 });
 
+// Update a book
 app.MapPut("/book/{id}", async (DataContext context, Book updatedBook, int id) =>
 {
     var book = await context.Books.FindAsync(id);
@@ -46,6 +53,7 @@ app.MapPut("/book/{id}", async (DataContext context, Book updatedBook, int id) =
     return Results.Ok(await context.Books.ToListAsync());
 });
 
+// Delete a book
 app.MapDelete("/book/{id}", async (DataContext context, int id) =>
 {
     var book = await context.Books.FindAsync(id);
@@ -58,8 +66,10 @@ app.MapDelete("/book/{id}", async (DataContext context, int id) =>
     return Results.Ok(await context.Books.ToListAsync());
 });
 
+
 app.Run();
 
+// model of a book
 public class Book
 {
     public int Id { get; set; }
